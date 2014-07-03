80179be82dd6aa525896f4cea0b11f62138c431f
maswhitaker <masforwhi@yahoo.com>
2014-07-03 15:05:02 -0400

    added backgrounds and victory page

--- monster-mash-game/app/scripts/main.js
+++ monster-mash-game/app/scripts/main.js
@@ -1,8 +1,5 @@
 /* Use strict */
 
-var infolog = $("#info").html();
-$("#monster-health").append(_.template(infolog,monster1));
-
 // CONSTRUCTORS
 // Here is where you'll create your constructors for you player and monster.
 // e.g. function Monster(){};
@@ -12,13 +9,16 @@ function Player(options){
   this.name = options.name;
   this.health = options.health;
   this.attack = function(monsta){
-    return monsta.health = monsta.health - (Math.random() * 20);
+    return monsta.health = monsta.health - (Math.random() * 10);
   };
 };
 function Monster(options){
   this.health = options.health;
   this.name = options.name;
-}
+  this.fight = function(player){
+    return player.health = player.health - (Math.random() * 8);
+  };
+};
 
 // GLOBAL VARIABLES
 // You'll also want to create variables for the specific players and monsters.
@@ -42,6 +42,10 @@ var monster1 = new Monster({
   name: "Ugly",
   health: 100
 });
+var monster2 = new Monster({
+  name: "Even Uglier",
+  health: 200
+});
 
 
 // ACTIONS
@@ -54,35 +58,73 @@ image1 = "images/images.duckduckgo.gif"
 image2 = "images/images.duckduckgo.jpg"
 image3 = "images/images.duckduckgo-1.jpg"
 
+backgroundImage1 = "images/dino-background-1.jpg"
+backgroundImage2 = "images/dino-background-2.jpg"
+backgroundImage3 = "images/dino-background-3.jpg"
+
 $('#img1').click(function(){
   $('#character').css('background-image','url('+ image1 +')');
   $('header').css('display', 'none');
   $('#after-char-select').css('display','initial');
   $('#char-name').html(dino1.name);
+  $('#background').css('background-image','url('+ backgroundImage1 +')');
 });
 $('#img2').click(function(){
   $('#character').css('background-image','url('+ image2 +')');
   $('header').css('display', 'none');
   $('#after-char-select').css('display','initial');
   $('#char-name').html(dino2.name);
+  $('#background').css('background-image','url('+ backgroundImage2 +')');
 });
 $('#img3').click(function(){
   $('#character').css('background-image','url('+ image3 +')');
   $('header').css('display', 'none');
   $('#after-char-select').css('display','initial');
   $('#char-name').html(dino3.name);
+  $('#background').css('background-image','url('+ backgroundImage3 +')');
 });
 $('#after-char-select').click(function(){
   $('#after-char-select').css('display','none');
   $('#battle').css('display','initial');
 });
 
+$('#dino-health').html(dino1.health);
+$('#dino-health').html(dino2.health);
+$('#dino-health').html(dino3.health);
+
 $('#fightFunction').click(function(){
   dino1.attack(monster1);
+  $('#monster-health').html(monster1.health);
+  setTimeout(function(){
+    monster1.fight(dino1);
+  },2000);
+  $('#dino-health').html(dino1.health);
+  if(monster1.health < 0){
+    $('#battle').css('display','none');
+    $('#victoryPage').css('display','initial');
+  }
 });
 $('#fightFunction').click(function(){
   dino2.attack(monster1);
+  $('#monster-health').html(monster1.health);
+  setTimeout(function(){
+    monster1.fight(dino2);
+  },2000);
+  $('#dino-health').html(dino2.health);
+  if(monster1.health < 0){
+    $('#battle').css('display','none');
+    $('#victoryPage').css('display','initial');
+  }
 });
 $('#fightFunction').click(function(){
   dino3.attack(monster1);
+  $('#monster-health').html(monster1.health);
+  setTimeout(function(){
+    monster1.fight(dino3);
+  },2000);
+  $('#dino-health').html(dino3.health);
+  if(monster1.health < 0){
+    $('#battle').css('display','none');
+    $('#victoryPage').css('display','initial');
+  }
 });
